57f3a6db896553dd937072d6dd10bccbbf317559
shaojunqian <201250099@smail.nju.edu.cn>
2023-10-12 15:09:17 +0800

    second test

--- src/Project.java
+++ src/Project.java
@@ -2,9 +2,18 @@ public class Project {
     public int aaa(){
         class A {
             public void printABC(){
+                System.out.println(123);
+            }
+        }
+        class B {
+            public void printABC(){
+                int d = 1;
+
                 System.out.println("A");
                 System.out.println("B");
                 System.out.println("C");
+
+                System.out.println(d);
             }
         }
         A a = new A();
